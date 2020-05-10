5610feba08aaf77fd9e9e6635844cbf5839600ab
Omikhleia <didier.willis@gmail.com>
2020-05-10 19:33:17 +0200

    hydroponics: new textures and fixes

--- machines/hydroponics.lua
+++ machines/hydroponics.lua
@@ -10,15 +10,15 @@ minetest.register_node("mt_future:hydroponic_substrate_empty", {
      -- top, bottom, right, left, back, front
     "wool_white.png",
     "pipeworks_storage_tank_back.png",
-    pipeworks.liquid_texture.."^pipeworks_storage_tank_front_0.png",
-    pipeworks.liquid_texture.."^pipeworks_storage_tank_front_0.png",
-    pipeworks.liquid_texture.."^pipeworks_storage_tank_front_0.png",
-    pipeworks.liquid_texture.."^pipeworks_storage_tank_front_0.png"
+    "mt_future_hydroponics_0.png",
+    "mt_future_hydroponics_0.png",
+    "mt_future_hydroponics_0.png",
+    "mt_future_hydroponics_0.png"
   },
   paramtype = "light",
   paramtype2 = "facedir",
   groups = {cracky=1, pipe=1},
-  pipe_connections = { left=1, right=1, front=1, back=1 },
+  pipe_connections = { front=1, back=1, right=1, left=1 },
 
   sounds = default.node_sound_metal_defaults(),
   after_place_node = function(pos)
@@ -27,36 +27,40 @@ minetest.register_node("mt_future:hydroponic_substrate_empty", {
   after_dig_node = function(pos)
     pipeworks.scan_for_pipe_objects(pos)
   end,
-  on_place = pipeworks.rotate_on_place,
-  on_rotate = pipeworks.fix_after_rotation
+  on_rotate = false
 })
-pipeworks.flowables.register.simple("mt_future:hydroponic_substrate_empty")
+-- FIXME: API says not to use any longer directional_horizonal_rotate()
+-- But no clear example how to do it otherwise.
+pipeworks.flowables.register.directional_horizonal_rotate("mt_future:hydroponic_substrate_empty", true)
 
 minetest.register_node("mt_future:hydroponic_substrate_loaded", {
   description = "Wet hydroponic substrate",
   tiles = {
-      -- top, bottom, right, left, back, front
-      "wool_white.png",
-      "pipeworks_storage_tank_back.png",
-      pipeworks.liquid_texture.."^pipeworks_storage_tank_front_10.png",
-      pipeworks.liquid_texture.."^pipeworks_storage_tank_front_10.png",
-      pipeworks.liquid_texture.."^pipeworks_storage_tank_front_10.png",
-      pipeworks.liquid_texture.."^pipeworks_storage_tank_front_10.png"
-    },
+    -- top, bottom, right, left, back, front
+    "wool_white.png",
+    "pipeworks_storage_tank_back.png",
+    pipeworks.liquid_texture.."^mt_future_hydroponics_10.png",
+    pipeworks.liquid_texture.."^mt_future_hydroponics_10.png",
+    pipeworks.liquid_texture.."^mt_future_hydroponics_10.png",
+    pipeworks.liquid_texture.."^mt_future_hydroponics_10.png"
+  },
   paramtype = "light",
   paramtype2 = "facedir",
   groups = {cracky=1, pipe=1, soil=3, not_in_creative_inventory=1},
-  pipe_connections = { left=1, right=1, front=1, back=1 },
+  pipe_connections = { front=1, back=1, right=1, left=1 },
   sounds = default.node_sound_metal_defaults(),
 
   after_dig_node = function(pos)
     pipeworks.scan_for_pipe_objects(pos)
   end,
   drop = "mt_future:hydroponic_substrate_empty",
+  on_rotate = false
 })
-pipeworks.flowables.register.simple("mt_future:hydroponic_substrate_loaded")
+-- FIXME: API says not to use any longer directional_horizonal_rotate()
+-- But no clear example how to do it otherwise.
+pipeworks.flowables.register.directional_horizonal_rotate("mt_future:hydroponic_substrate_loaded", true)
 
--- ABMs are not registered automatically by pipeworks
+-- ABMs are not registered automatically by pipeworks?
 minetest.register_abm({
   nodenames = { "mt_future:hydroponic_substrate_empty" },
   interval = 1,
